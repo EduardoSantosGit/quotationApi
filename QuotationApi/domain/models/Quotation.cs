91c8d139044e33b55ab039cc28c56344487e51c8
EduardoSantosGit <eduardosantos058@gmail.com>
2017-08-27 21:07:58 -0300

    init

---
+++ QuotationApi/domain/models/Quotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace QuotationApi.domain.models
+{
+    public class Quotation
+    {
+        public string Index { get; set; }
+        public string Parents { get; set; }
+        public string Point { get; set; }
+        public string Hour { get; set; }
+    }
+}
---
+++ QuotationApi/domain/services/api/QuotationServicesApi.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace QuotationApi.domain.services.api
+{
+    public class QuotatioServicesApi
+    {
+        private HttpClient _httpClient;
+        
+        public async Task<string> GetPageIndex()
+        {
+            try
+            {
+                _httpClient = new HttpClient();
+                var response = await _httpClient.GetAsync("http://exame.abril.com.br/");
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    return response.Content.ReadAsStringAsync().Result;
+                }
+            }
+            catch (Exception ex)
+            {
+                return "";
+            }
+            return "";
+        }
+
+        public async Task<string> GetPageQuotaton()
+        {
+            try
+            {
+                _httpClient = new HttpClient();
+                var response = await _httpClient.GetAsync("http://exame.advfn.com/");
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    return response.Content.ReadAsStringAsync().Result;
+                }
+            }
+            catch (Exception ex)
+            {
+                return "";
+            }
+            return "";
+        }
+    }
+}
