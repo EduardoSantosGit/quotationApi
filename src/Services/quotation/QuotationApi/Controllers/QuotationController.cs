dfb7945b0b4a481f4337d0e624753e0e2bd71029
EduardoSantosGit <eduardosantos058@gmail.com>
2017-09-10 21:30:11 -0300

    alter directory solution

---
+++ src/Services/quotation/QuotationApi/Controllers/QuotationController.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QuotationApi.domain.services;
+
+namespace QuotationApi.Controllers
+{
+    [Route("api")]
+    public class QuotationController : Controller
+    {       
+        [HttpGet("quotation/coin")]
+        public async Task<IActionResult> GetCoins()
+        {
+            var quotation = new QuotationService();
+            var page = await quotation.GetValues();
+            return Ok(page);
+        }
+        
+        [HttpGet("quotation/world")]
+        public async Task<IActionResult> GetQuotations()
+        {
+            var quotation = new QuotationService();
+            var page = await quotation.GetValuesQuotation();           
+            return Ok(page);
+        }
+        
+        
+    }
+    
+    
+}
