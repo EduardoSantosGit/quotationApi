d75cb1d8a8a6ba1056d3b397c9a22bebbf6fbafa
EduardoSantosGit <eduardosantos058@gmail.com>
2017-09-24 17:22:40 -0300

    implement method clippingblock

--- src/Services/markets/WorldMarkets.Domain/Common/ScrapParser.cs
+++ src/Services/markets/WorldMarkets.Domain/Common/ScrapParser.cs
@@ -16,5 +16,14 @@
             var block = html.Substring(fisrtIndex, exit);
             return block;
         }
+
+        public string ClippingBlock(string block, string indexOn, string indexLast)
+        {
+            var firstIndex = block.IndexOf(indexOn);
+            var lastIndex = block.IndexOf(indexLast);
+            var count = lastIndex - firstIndex;
+            var resultBlock = block.Remove(firstIndex, count);
+            return resultBlock;
+        }
     }
 }
